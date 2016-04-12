44fe37462b2c40b8e9f229501f2b5a074a9e383f
dscroft <dscroft@gmail.com>
2016-04-12 16:05:26 +0100

    Revert "Auto commit"
    This reverts commit c1bbb563f173de3df2d0b494b27e184ea441aa9c.
    
    Was supposed to go to codio branch

--- lab_array.cpp
+++ lab_array.cpp
@@ -6,49 +6,49 @@
 using namespace std;
 
 /* Getting input from the user in C++ is simply a matter
-    of saying:
-        int input;
-        cin >> input;
-    However, getting C++ to handle invalid input is 
-    surprisingly complex. So here is a input() function
-    that I have written that does it all for you.
-    It behaves in a similar way to input() in Python
-    and will raise a runtime_error exception if the user
-    trys to enter anything that can't be converted to the
-    correct type. You don't need to know how this work at
-    the moment, just type...
-        
-        input<int>();
+	of saying:
+		int input;
+		cin >> input;
+	However, getting C++ to handle invalid input is 
+	surprisingly complex. So here is a input() function
+	that I have written that does it all for you.
+	It behaves in a similar way to input() in Python
+	and will raise a runtime_error exception if the user
+	trys to enter anything that can't be converted to the
+	correct type. You don't need to know how this work at
+	the moment, just type...
+		
+		input<int>();
 
-    ... to read in ints or ...
-        
-        input<float>();
+	... to read in ints or ...
+		
+		input<float>();
 
-    ..if you watch to read floats etc. . */
+	..if you watch to read floats etc. . */
 template<typename T>
 T input()
 {
-    string buffer;
-    getline(cin, buffer);
+	string buffer;
+	getline(cin, buffer);
 
-    stringstream ss(buffer);
-    T input;
+	stringstream ss(buffer);
+	T input;
 
-    if( ss >> input && ss.eof() )
-        return input;
+	if( ss >> input && ss.eof() )
+		return input;
 
-    throw runtime_error("Input is of invalid type");
+	throw runtime_error("Input is of invalid type");
 }
 
 int main()
 {
-    vector<int> userinputs;
+	vector<int> userinputs;
 
-    // COMPLETE ME
+	// COMPLETE ME
 
-    int ui = input<int>();
+	int ui = input<int>();
 
-    // COMPLETE ME
+	// COMPLETE ME
 
-    return 0;
+	return 0;
 }
