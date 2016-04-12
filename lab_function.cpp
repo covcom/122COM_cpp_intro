c1bbb563f173de3df2d0b494b27e184ea441aa9c
dscroft <dscroft@gmail.com>
2016-04-12 16:00:23 +0100

    Auto commit

--- lab_function.cpp
+++ lab_function.cpp
@@ -10,27 +10,27 @@ using namespace std;
 
 int main()
 {
-	int errors = 0;
-	for( int year=1800; year<2101; ++year )
-	{	
-		// deliberate gibberish so that you can't just copy the answer
-		bool correctAnswer = (!(year%('b'+'e'+'c'+'f'))&&!(year%'d'))||(!(year&('R'-'O'/*+'Q'+'T'*/))&&(year%'d'))||(!(year%('d'+'d'+'d'+'d'))&&(year&('*'-'\''))&&(year%('N'-'\'')));
-		bool yourAnswer = leap_year( year );
+    int errors = 0;
+    for( int year=1800; year<2101; ++year )
+    {    
+        // deliberate gibberish so that you can't just copy the answer
+        bool correctAnswer = (!(year%('b'+'e'+'c'+'f'))&&!(year%'d'))||(!(year&('R'-'O'/*+'Q'+'T'*/))&&(year%'d'))||(!(year%('d'+'d'+'d'+'d'))&&(year&('*'-'\''))&&(year%('N'-'\'')));
+        bool yourAnswer = leap_year( year );
 
-		if( correctAnswer != yourAnswer )
-		{
-			cerr << "Error! leap_year() says " << year 
-				<< (yourAnswer ? " is " : " is not ") << "a leap year but it "
-				<< (correctAnswer ? " is" : " is not") << "." << endl;
+        if( correctAnswer != yourAnswer )
+        {
+            cerr << "Error! leap_year() says " << year 
+                << (yourAnswer ? " is " : " is not ") << "a leap year but it "
+                << (correctAnswer ? " is" : " is not") << "." << endl;
 
-			errors++;
-		}
-	}
+            errors++;
+        }
+    }
 
-	if( errors == 0 )
-		cout << "Congratulations, no errors" << endl;
-	else
-		cout << "Uh oh, " << errors << " error/s remain" << endl;
+    if( errors == 0 )
+        cout << "Congratulations, no errors" << endl;
+    else
+        cout << "Uh oh, " << errors << " error/s remain" << endl;
 
-	return 0;
+    return errors;
 }
--- pre_error.cpp
+++ pre_error.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main()
 {
-	int bugs = 1;
-	for( int i=0; i>10; ++i )
-	{
-		cout << bugs << " little bugs in the code" << endl;
-		cout << bugs << " little bugs" << endl;
-		cout << "Take one down, patch it around" << endl
-	
-		bugs *= 2;
+    int bugs = 1;
+    for( int i=0; i>10; ++i )
+    {
+        cout << bugs << " little bugs in the code" << endl;
+        cout << bugs << " little bugs" << endl;
+        cout << "Take one down, patch it around" << endl
+    
+        bugs *= 2;
 
-		cout < bugs << " little bugs in the code..." << enld;
-		cout << endl;
-	
+        cout < bugs << " little bugs in the code..." << enld;
+        cout << endl;
+    
 
-	return 0;
+    return 0;
 }
