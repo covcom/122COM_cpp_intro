971bc950854ce15da3843f4b810a0c5857a853f2
dscroft <dscroft@gmail.com>
2016-04-12 16:21:28 +0100

    Auto commit

---
+++ lab_function.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <stdexcept>
+using namespace std;
+
+// COMPLETE ME
+// PUT THE leap_year() FUNCTION THAT YOU WRITE HERE
+
+int main()
+{
+    int errors = 0;
+    for( int year=1800; year<2101; ++year )
+    {    
+        // deliberate gibberish so that you can't just copy the answer
+        bool correctAnswer = (!(year%('b'+'e'+'c'+'f'))&&!(year%'d'))||(!(year&('R'-'O'/*+'Q'+'T'*/))&&(year%'d'))||(!(year%('d'+'d'+'d'+'d'))&&(year&('*'-'\''))&&(year%('N'-'\'')));
+        bool yourAnswer = leap_year( year );
+
+        if( correctAnswer != yourAnswer )
+        {
+            cerr << "Error! leap_year() says " << year 
+                << (yourAnswer ? " is " : " is not ") << "a leap year but it "
+                << (correctAnswer ? " is" : " is not") << "." << endl;
+
+            errors++;
+        }
+    }
+
+    if( errors == 0 )
+        cout << "Congratulations, no errors" << endl;
+    else
+        cout << "Uh oh, " << errors << " error/s remain" << endl;
+
+    return errors;
+}
