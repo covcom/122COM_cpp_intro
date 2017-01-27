b9d9312fd63a849b1852e4cf9f20e0f443cd85db
david <dscroft@gmail.com>
2017-01-27 13:44:19 +0000

    Auto commit

--- lab_array.py
+++ lab_array.py
@@ -3,15 +3,15 @@ import sys
 def main():
     userinputs = []
 
-    while len(userinputs) < 5:
+    while len(userinputs) < 5:                    # read in the 5 integers
         try:
             ui = int(input('Enter integer:'))
             userinputs.append(ui)
-        except ValueError:
+        except ValueError:                        # handle any non-integers
             print('Not an integer')
         
-    for i in range(len(userinputs)-1, -1, -1):
-        print( 'number', i+1, 'is', userinputs[i] )
+    for i in range(len(userinputs)-1, -1, -1):    # print them back out in reverse order
+        print( 'number {} is {}'.format( i+1, userinputs[i] ) )
 
 if __name__ == '__main__':
     sys.exit(main())
