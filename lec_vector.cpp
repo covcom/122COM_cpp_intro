73ad69022b15f224cae4cfba1f3d0621fe7a2a45
voodoo <dscroft@gmail.com>
2017-02-04 17:04:11 +0000

    Auto commit

--- lec_vector.cpp
+++ lec_vector.cpp
@@ -8,7 +8,7 @@ int main()
     array<int,5> myArray = {1,2,3,4,5};
     vector<int> myVector = {1,2,3,4};
     
-    myVector.push_back(5);
+    myVector.emplace_back(5);
     
     cout << myArray[0] << endl;
     cout << myVector[0] << endl;
